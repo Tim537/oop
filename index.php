88ad963795549c3025ee9354bda8650bd00a2983
Tim <selectdev@gmail.com>
2017-01-09 15:24:16 -0500

    Repo Setup

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php include("includes/header.php"); ?>
+
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-sm-8">
+
+    
+            
+          Shit goes here
+         
+
+            </div>
+
+
+
+
+            <!-- Blog Sidebar Widgets Column -->
+            <div class="col-sm-4">
+
+            
+                 <?php include("includes/sidebar.php"); ?>
+
+
+
+        </div>
+        <!-- /.row -->
+
+        <?php include("includes/footer.php"); ?>
