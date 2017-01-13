c533d2b1ea5e70292074edce6e3eefe036f862cb
Tim <selectdev@gmail.com>
2017-01-13 13:21:43 -0500

    Added db_object class, made user extend it, and moved some user methods to it

--- admin/includes/User.php
+++ admin/includes/User.php
@@ -2,7 +2,7 @@
 
 // User Class
 
-class User
+class User extends Db_object
 {
     protected static $db_table = "users";
     protected static $db_table_fields = array('username', 'password', 'first_name', 'last_name');
@@ -13,31 +13,9 @@ class User
     public $last_name;
 
 
-    // Get all the users
-    public static function find_all() {
-        return self::find_this_query("SELECT * FROM " . self::$db_table);
-    }
 
-    // Get a user with a specified id
-    public static function find_by_id($id) {
-        $the_result_array = self::find_this_query("SELECT * FROM " . self::$db_table . " WHERE id = $id LIMIT 1");
 
-        return !empty($the_result_array) ? array_shift($the_result_array) : false;
 
-    }
-
-    // Run the queries and return the result
-    public static function find_this_query($sql) {
-        global $database;
-        $result = $database->query($sql);
-        $the_object_array = array();
-
-        while ($row = mysqli_fetch_array($result)) {
-            $the_object_array[] = self::instantiation($row);
-        }
-
-        return $the_object_array;
-    } // find_this_query
 
     public static function verify_user($username, $password) {
         global $database;
@@ -51,24 +29,9 @@ class User
         return !empty($the_result_array) ? array_shift($the_result_array) : false;
     } // verify_user
 
-    // Puts result rows into an object
-    public static function instantiation($the_record) {
-        $the_object = new self;
 
-        foreach ($the_record as $the_attribute => $value) {
-            if ($the_object->has_the_attribute($the_attribute)) {
-                $the_object->$the_attribute = $value;
-            }
-        }
 
-        return $the_object;
-    } // instantiation
 
-    // Checks to see if the property exists in the class
-    private function has_the_attribute($the_attribute) {
-        $object_properties = get_object_vars($this);
-        return array_key_exists($the_attribute, $object_properties);
-    } // has_the_attribute
 
     // Returns all object properties
     protected function properties() {
---
+++ admin/includes/db_object.php
@@ -0,0 +1,51 @@
+<?php
+
+class Db_object
+{
+// Get all the users
+    public static function find_all() {
+        return self::find_this_query("SELECT * FROM " . self::$db_table);
+    }
+
+    // Get a user with a specified id
+    public static function find_by_id($id) {
+        $the_result_array = self::find_this_query("SELECT * FROM " . self::$db_table . " WHERE id = $id LIMIT 1");
+
+        return !empty($the_result_array) ? array_shift($the_result_array) : false;
+
+    }
+
+    // Run the queries and return the result
+    public static function find_this_query($sql) {
+        global $database;
+        $result = $database->query($sql);
+        $the_object_array = array();
+
+        while ($row = mysqli_fetch_array($result)) {
+            $the_object_array[] = self::instantiation($row);
+        }
+
+        return $the_object_array;
+    } // find_this_query
+
+    // Puts result rows into an object
+    public static function instantiation($the_record) {
+        $the_object = new self;
+
+        foreach ($the_record as $the_attribute => $value) {
+            if ($the_object->has_the_attribute($the_attribute)) {
+                $the_object->$the_attribute = $value;
+            }
+        }
+
+        return $the_object;
+    } // instantiation
+
+    // Checks to see if the property exists in the class
+    private function has_the_attribute($the_attribute) {
+        $object_properties = get_object_vars($this);
+        return array_key_exists($the_attribute, $object_properties);
+    } // has_the_attribute
+
+
+} // End Class
