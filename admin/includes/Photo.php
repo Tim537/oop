967e66f0c8580daf606678fb3d7508c2addb9198
Tim <selectdev@gmail.com>
2017-01-16 09:48:51 -0500

    Added save method to photo class

--- admin/includes/Photo.php
+++ admin/includes/Photo.php
@@ -28,22 +28,55 @@ class Photo extends Db_object
     );
 
     // This is passed $_FILES['uploaded_file'] as an argument
-    public function set_file($file){
+    public function set_file($file) {
 
-        if(empty($file) || !$file || !is_array($file)){
+        if (empty($file) || !$file || !is_array($file)) {
             $this->errors = "There was no file uploaded";
             return false;
-        }elseif($file['error'] != 0){
+        } elseif ($file['error'] != 0) {
             $this->errors[] = $this->upload_errors_array[$file['error']];
             return false;
-        }else{
+        } else {
             $this->filename = basename($file['name']);
-            $this->tmp_path = basename($file['tmp_name']);
-            $this->type = basename($file['type']);
-            $this->size = basename($file['size']);
+            $this->tmp_path = $file['tmp_name'];
+            $this->type = $file['type'];
+            $this->size = $file['size'];
             return true;
         }
     } // set_file()
+
+    public function save() {
+        if ($this->photo_id) {
+            $this->update();
+        } else {
+            if (!empty($this->errors)) {
+                return false;
+            }
+
+            if (empty($this->filename) || empty($this->tmp_path)) {
+                $this->errors[] = "The file was not available";
+                return false;
+            }
+
+            $target_path = SITE_ROOT . DS . 'admin' . DS . $this->upload_directory . DS . $this->filename;
+
+            if (file_exists($target_path)) {
+                $this->errors[] = "The file {$this->filename} already exists";
+                return false;
+            }
+
+            // Move the file
+            if (move_uploaded_file($this->tmp_path, $target_path)) {
+                if ($this->create()) {
+                    unset($this->tmp_path);
+                    return true;
+                }
+            } else {
+                $this->errors[] = "The folder probably does not have permission";
+                return false;
+            }
+        }
+    } // save()
 
 
 } // End Class
