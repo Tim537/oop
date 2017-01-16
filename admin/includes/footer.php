945a08a593035664e3ed6056bf270fca09ac8c3f
Tim <selectdev@gmail.com>
2017-01-16 13:22:22 -0500

    Added js/scripts.js and tinymce editor

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,11 +1,14 @@
-  </div>
-    <!-- /#wrapper -->
+</div>
+<!-- /#wrapper -->
 
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+<!--  TinyMCE wysiwyg editor  -->
+<script src="http://cdn.tinymce.com/4/tinymce.min.js"></script>
+<script src="js/scripts.js"></script>
 
 </body>
 
