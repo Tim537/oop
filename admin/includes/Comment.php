1f3196947c0435315a5db8e99d8671e69b75d50f
Tim <selectdev@gmail.com>
2017-01-17 12:00:49 -0500

    Added create and find methods

--- admin/includes/Comment.php
+++ admin/includes/Comment.php
@@ -11,7 +11,27 @@ class Comment extends Db_object
     public $author;
     public $body;
 
+    // Returns comment object with the given properties set
+    public static function create_comment($photo_id, $author, $body){
+        if(!empty($photo_id) && !empty($author) && !empty($body)){
+            $comment = new Comment();
+            $comment->photo_id = (int)$photo_id;
+            $comment->author = $author;
+            $comment->body = $body;
 
+            return $comment;
+        }else{
+            return false;
+        }
+    } // create_comment()
+
+    // Find comments by photo_id
+    public static function find_the_comments($photo_id){
+        global $database;
+        $photo_id = $database->escape_string($photo_id);
+        $sql = "SELECT * FROM ". self::$db_table ." WHERE photo_id = $photo_id ORDER BY id DESC";
+        return self::find_by_query($sql);
+    }
 
 
 } // End of Class
