9a641901b0379aa4d0f032945d4ad2d234d08fe4
Tim <selectdev@gmail.com>
2017-01-18 10:11:55 -0500

    Aded Paginate class

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php include("includes/header.php"); ?>
 <?php
+
+// Pagination
+$page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
+$items_per_page = 4;
+$items_total_count = Photo::count_all();
+
 $photos = Photo::find_all();
 ?>
 
