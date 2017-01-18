d1c2e10b9c0bc59ad52314e6cd17dbad922d9e59
Tim <selectdev@gmail.com>
2017-01-18 12:57:15 -0500

    Complete paginate class and added prev/next buttons to index.php

--- admin/includes/Paginate.php
+++ admin/includes/Paginate.php
@@ -2,5 +2,44 @@
 
 class Paginate
 {
+    public $current_page;
+    public $items_per_page;
+    public $items_total_count;
 
-}
+    public function __construct($page = 1, $items_per_page = 4, $items_total_count = 0) {
+        $this->current_page = (int)$page;
+        $this->items_per_page = (int)$items_per_page;
+        $this->items_total_count = (int)$items_total_count;
+    } // __construct()
+
+    // Return next page
+    public function next() {
+        return $this->current_page + 1;
+    } // next()
+
+    // Return previous page
+    public function previous() {
+        return $this->current_page - 1;
+    } // previous()
+
+    // Return total number of pages
+    public function page_total() {
+        return ceil($this->items_total_count / $this->items_per_page);
+    } // page_total()
+
+    // Return bool if previous page
+    public function has_previous(){
+        return $this->previous() >= 1 ? true : false;
+    } // has_previous()
+
+    // Return bool if next page
+    public function has_next(){
+        return $this->next() <= $this->page_total() ? true : false;
+    } // has_next()
+
+    // Returns the offset
+    public function offset(){
+        return ($this->current_page - 1) * $this->items_per_page;
+    } // offset()
+
+} // End Class
--- index.php
+++ index.php
@@ -6,7 +6,16 @@ $page = !empty($_GET['page']) ? (int)$_GET['page'] : 1;
 $items_per_page = 4;
 $items_total_count = Photo::count_all();
 
-$photos = Photo::find_all();
+$paginate = new Paginate($page, $items_per_page, $items_total_count);
+
+$sql = "SELECT * FROM photos ";
+$sql .= "ORDER BY id DESC ";
+$sql .= "LIMIT {$paginate->items_per_page} ";
+$sql .= "OFFSET {$paginate->offset()}";
+
+$photos = Photo::find_by_query($sql);
+
+//$photos = Photo::find_all();
 ?>
 
 <div class="row">
@@ -22,9 +31,25 @@ $photos = Photo::find_all();
                     </a>
                 </div>
 
-
             <?php endforeach; ?>
+        </div>
+        <div class="row">
+            <ul class="pager">
+                <?php
+                if ($paginate->page_total() > 1) {
+                    // Previous Button
+                    if ($paginate->has_previous()) {
+                        echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
+                    }
+
+                    //Next button
+                    if ($paginate->has_next()) {
+                        echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
+                    }
+                }
+                ?>
+            </ul>
         </div>
     </div>
 </div>
-    <?php include("includes/footer.php"); ?>
+<?php include("includes/footer.php"); ?>
